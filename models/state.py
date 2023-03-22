9866ce9a09aa532c343ae0b71c14ec4da5fa1c97
cooldebs <doduronbi83@gmail.com>
2023-03-22 13:13:50 -0700

    Sql

--- models/state.py
+++ models/state.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import models
+from models.city import city
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+
