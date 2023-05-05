1ddb377dce234699d4c8d6954c08062ae1f7354f
Chisom Daniel <chinwezechisom@gmail.com>
2023-05-05 16:24:53 +0100

    task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 # Setting up a web server
 # make sure you run the file with sudo priviledges
+# shellcheck disable=SC2154
+# shellcheck disable=SC2230
 
-# update and upgrade the shell
-apt update -y && sudo apt upgrade -y
+check=$(which nginx | grep '/usr/bin/nginx')
+if [ -z "$check" ]; then
+	# update and upgrade the shell
+	apt update -y && sudo apt upgrade -y
+	
+	# install nginx
+	apt install nginx -y
+fi
 
-# install nginx
-apt install nginx -y
 
 # adjust the fire wall
 ufw allow 'Nginx HTTP'
 
-
+# create the files if they don't exist
 if [ ! -d "/data/web_static/releases/test/" ]; then
 	mkdir -p /data/web_static/releases/test/
 fi
@@ -32,9 +38,16 @@ else
 fi
 
 chown -R ubuntu:ubuntu /data/
-#sudo chmod -R 755 /var/www
 
+check2=$(grep 'location /hbnb_static/' /etc/nginx/sites-available/default)
+if [ -z "$check2" ]; then
+	string="server_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\ttry_files $uri $uri/ =404;\n}\n"
+	sed -i "s|server_name _;|$string|" /etc/nginx/sites-available/default
+fi
 
+#chmod -R 755 /data
 
 # restart nginx
 service nginx restart
+
+exit 0
