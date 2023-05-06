e1b0b03d7493c1a0385ffbde506c75ace6a34c91
Chisom Daniel <chinwezechisom@gmail.com>
2023-05-06 19:32:32 +0100

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Using Fabric to carryout operation on our terminal'''
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    dt = datetime.now()
+    timestamp = f"{dt.year}{dt.month}{dt.day}{dt.hour}{dt.minute}{dt.second}"
+
+    if not os.path.exists('versions'):
+        os.mkdir('versions')
+
+    output = local(f"tar -cvzf versions/web_static_{timestamp}.tgz web_static")
+
+    if (output.failed is False):
+        return (f"versions/web_static_{timestamp}.tgz")
+    else:
+        return (None)
