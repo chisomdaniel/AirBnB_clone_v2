b52ecef89f6f21dda7964bb551cd2b6ff22da88e
Chisom Daniel <chinwezechisom@gmail.com>
2023-05-18 17:03:39 +0100

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 '''Using Fabric to carryout operation on our terminal'''
 from fabric.api import local
 from datetime import datetime
@@ -8,7 +9,11 @@ import os
 def do_pack():
     """Generates a .tgz archive file
 
-    Return: the archive path
+    All files in the folder web_static must be added to the final archive
+    All archives must be stored in the folder versions
+    (your function should create this folder if it doesn’t exist)
+    Return the archive path if the archive has been correctly generated.
+    Otherwise, it should return None
     """
 
     dt = datetime.now()
@@ -23,4 +28,3 @@ def do_pack():
         return (f"versions/web_static_{timestamp}.tgz")
     else:
         return (None)
-
